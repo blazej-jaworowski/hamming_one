73b33719136be031d323fa6aa7319e9881a7ecb2
blazej-jaworowski <blazej.jaworowski@gmail.com>
2022-01-28 22:25:37 +0100

    Fixed generating sequences

--- generate_sequences.sh
+++ generate_sequences.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
-build/test_generator 10000 1000 1000
-build/test_generator 20000 1000 2000
-build/test_generator 40000 1000 4000
-build/test_generator 80000 1000 8000
-build/test_generator 160000 1000 16000
+build/test_generator 1000 10000 1000
+build/test_generator 1000 20000 2000
+build/test_generator 1000 40000 4000
+build/test_generator 1000 80000 8000
+build/test_generator 1000 160000 16000
 
-build/test_generator 10000 2000 1000
-build/test_generator 20000 2000 2000
-build/test_generator 40000 2000 4000
-build/test_generator 80000 2000 8000
-build/test_generator 160000 2000 16000
+build/test_generator 2000 10000 1000
+build/test_generator 2000 20000 2000
+build/test_generator 2000 40000 4000
+build/test_generator 2000 80000 8000
+build/test_generator 2000 160000 16000
